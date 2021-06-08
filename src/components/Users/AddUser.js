43327fc7f44a098ac901bfdd30ed4a1dfd9760e8
Reid McCulloch <rmcc0724@gmail.com>
2021-06-07 23:58:29 -0700

    Project started

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Button from '../../Button.js';
+import Card from '../UI/Card.js';
+import classes from './AddUser.module.css';
+
+
+const AddUser = (props) => {
+
+  const addUserHandler = (event => {
+    event.preventDefaault();
+  })
+
+    return (
+      <Card className={classes.input}>
+          <form onSubmit={addUserHandler}>
+          <label htmlFor="username" >Username</label>
+          <input id="username" type="text" />
+          <label htmlFor="username" >Age (Years)</label>
+          <input id="age" type="number"/>
+          <Button type="submit">Add User</Button>
+      </form>
+      </Card>
+
+    );
+  }
+  
+  export default AddUser;
